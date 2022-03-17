2e0afeca4ea8a2e38ad4b7b12a69b3828da3b3c6
root <root@DESKTOP-LINP517.localdomain>
2022-03-17 23:40:49 +0300

    create mongo-database; append req to get files;

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express =  require('express');
 const app = express();
 
-app.use(express.static('./'));
+const {MongoClient} = require('mongodb');
+const client = new MongoClient('mongodb://localhost:27017');
+
+app.use(express.static(__dirname));
+
+app.use(async (req, res, next) => {
+    await client.connect();
+    req.db = client.db('commentNotes');
+
+    next();
+});
+
+app.get('/:fileName', async  (req, res, next) => {
+    const fileName = req.params.fileName;
+    const fileInfo = await req.db.collection('files').findOne({name: fileName});
+
+    res.status(200).send(fileInfo);
+});
 
 app.get('/', async (req, res, next) => {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + '/main.html');
 });
 
 app.listen(5500, () => {
     console.log('server started');
-})
+});
